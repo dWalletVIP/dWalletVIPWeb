54727fafbf62e85f961546cfb1976009634d487f
kylie95morrison <kylie.morrison.95@mail.ru>
2022-04-05 21:03:37 +0800

    deploy web : V0.01:init

---
+++ assets/welcome.html.a376a32f.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to dWallet.VIP",lang:"en-US",frontmatter:{index:1,title:"Welcome to dWallet.VIP",author:"dWallet.VIP",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`dWallet : Decentralized Wallet.
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://dwallet.vip/welcome.html"}],["meta",{property:"og:title",content:"Welcome to dWallet.VIP"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"dWallet.VIP"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>dWallet : Decentralized Wallet.</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"dWallet VIP",slug:"dwallet-vip",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why dWalletVIP?",slug:"why-dwalletvip",children:[]},{level:2,title:"Tech Stack to Build dWallet VIP",slug:"tech-stack-to-build-dwallet-vip",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:1649163773e3,updatedTime:1649163773e3,contributors:[{name:"kylie95morrison",email:"kylie.morrison.95@mail.ru",commits:1}]},readingTime:{minutes:.42,words:127},filePathRelative:"welcome.md"};export{e as data};
